6f338d122ebe0d86efca465f70c08fa1666d2626
Alex Orga <alexorga.dev@gmail.com>
2018-06-02 09:36:50 -0500

    getting movies back from being saved with iswatched button

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,6 +1,6 @@
 /* eslint camelcase: 0 */
 
-const domString = (movieArray, config, whereToPrint) => {
+const domString = (movieArray, config, whereToPrint, myCollectionMode = false) => {
   let string = '';
   movieArray.forEach((movie, index) => {
     if (index % 3 === 0) {
@@ -8,12 +8,20 @@ const domString = (movieArray, config, whereToPrint) => {
     }
     string += `<div class="col-sm-6 col-md-4">`;
     string +=  `<div class="thumbnail movie">`;
+    if (myCollectionMode) {
+      string += `<a class="btn deleteMovieFromCollectionEvent">X</a>`;
+    }
     string +=    `<img data-poster="${movie.poster_path}" src="${config.base_url}/w342/${movie.poster_path}" alt="Movie Poster">`;
     string +=    `<div class="caption">`;
     string +=      `<h3 class="movie-title">${movie.original_title ? movie.original_title : movie.title}</h3>`;
     string +=      `<p class="movie-overview">${movie.overview}</p>`;
-    string +=      `<p><a href="#" class="btn btn-primary" role="button">Review</a></p>`;
-    string +=      `<p><a href="#" class="btn btn-default addMovieToWishlist" role="button">Wishlist</a></p>`;
+    if (!myCollectionMode) {
+      string +=      `<p><a href="#" class="btn btn-default addMovieToWishlist" role="button">Wishlist</a></p>`;
+    } else if (myCollectionMode && !movie.isWatched) {
+      string += `<p><a class="btn btn-primary updateMovieToWatched" role="button">I've Watched It</a></p>`;
+    } else {
+      string += `<p>I'm going to put star rating here one day</p>`;
+    }
     string +=    `</div>`;
     string +=  `</div>`;
     string += `</div>`;
